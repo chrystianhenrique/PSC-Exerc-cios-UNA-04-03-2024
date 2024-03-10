88238dc1af2dc5155858f915181f1400e80f0f33
chrystianhenrique <161754767+chrystianhenrique@users.noreply.github.com>
2024-03-10 01:53:04 -0300

    Add files via upload
    Crie um algoritmo que leia o primeiro nome do usuário e escreva uma saudação para ele.

--- Exercício 3.java
+++ Exercício 3.java
@@ -3,17 +3,14 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
-        // Cria um objeto Scanner para ler a entrada do usuário
         Scanner scanner = new Scanner(System.in);
 
-        // Solicita ao usuário que digite seu primeiro nome
-        System.out.print("Digite seu primeiro nome: ");
+        System.out.print("Digite seu nome: ");
+
         String nome = scanner.next();
 
-        // Exibe uma saudação personalizada
         System.out.println("Olá, " + nome + "! Bem-vindo!");
 
-        // Fecha o Scanner para evitar vazamento de recursos
         scanner.close();
     }
 }
